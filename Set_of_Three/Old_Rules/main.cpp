30dbea64680ada63a78de28db683bfc2973ca9bb
Shaniamoro <33161479+Shaniamoro@users.noreply.github.com>
2019-03-16 23:21:38 -0400

    Create main.cpp

---
+++ Set_of_Three/Old_Rules/main.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+class Countdown {
+	int T = 0; // target number 
+	int a;
+	int b;
+  int c;
+  public:
+    Countdown();
+    Countdown(int, int, int,int);
+    double Addition(int, int);
+    double  Multiplication(int, int);
+    double  Division(int, int); // Only integers allowed 
+    double  Subtraction(int, int);// Only Positive Subtraction allowed 
+    bool Compare(int, int, int);
+    bool Compare(int, int, int,int);
+};
+Countdown::Countdown() {
+	T = 10;
+	a = 5;
+	b = 2;
+  c= 1;
+}
+
+// Instead of a and b maybe use an array
+
+
+Countdown::Countdown(int Target, int  first, int second,int third) {
+	T = Target;
+	a = first;
+	b = second;
+  c = third;
+}
+double Countdown::Addition(int a, int b) {
+	int sum = a + b;
+	return sum;
+}
+double Countdown::Multiplication(int a, int b) {
+	int mult = a * b;
+	return mult;
+}
+double Countdown::Division(int a, int b) {
+	int div = 0;
+  if (a>=b){
+	  if ( b!=0 && a % b == 0) {
+			div = a / b;
+    } else {
+    div = a/1;
+    }
+  } else if (a < b){
+      if( a!=0 && b% a == 0) {
+				div = b / a;
+			} else {
+        div=b/1;
+      }
+  }
+		return div;
+}
+double Countdown::Subtraction(int a, int b) {
+	int sub = 0;
+	if (a > b) {
+		sub = a - b;
+	}
+	else if (b > a) {
+		sub = b - a;
+	}
+
+	return sub;
+}
+
+bool Countdown::Compare(int T, int a, int b) {
+	if (Addition(a, b) == T) {
+	// 	outfile << "Solved " << T << endl;
+  //  outfile << "{ " << a << " , " << b <<" }"<< endl;
+		return true;
+	}
+	else if (Multiplication(a, b) == T) {
+	// 	outfile << "Solved " << T << endl;
+  //  outfile << "{ " << a << " , " << b <<" }"<< endl;
+		return true;
+	}
+	else if (Division(a, b) == T) {
+	// 	outfile << "Solved " << T << endl;
+  //  outfile << "{ " << a << " , " << b <<" }"<< endl;
+		return true;
+	}
+	else if (Subtraction(a, b) == T) {
+		// outfile << "Solved " << T << endl;
+    // outfile << "{ " << a << " , " << b <<" }"<< endl;
+		return true;
+	}
+
+	else {
+		return false;
+	}
+}
+bool Countdown::Compare(int T, int a, int b,int c) {
+  //Check if pairs of two can solve first
+  if(Compare(T,a,b)== true){
+    return true;
+  }
+  else if(Compare(T,b,c)==true){
+    return true;
+  }
+  else if(Compare (T,a,c)==true){
+    return true;
+  }
+  else if(Compare(T,Addition(a,b),c)==true){
+    return true;
+  }
+  else if (Compare(T,Addition(b,c),a)==true){
+    return true;
+  }
+  else if(Compare(T,Addition(a,c),b)==true){
+    return true;
+  }
+  else if(Compare(T,Multiplication(a,b),c)==true){
+    return true;
+  }
+  else if(Compare(T,Multiplication(b,c),a)==true){
+    return true;
+  }
+  else if(Compare(T,Multiplication(a,c),b)==true){
+    return true;
+  }
+  else if(Compare(T, Division(a,b),c)==true){
+    return true;
+  }
+  else if(Compare(T, Division(b,c),a)==true){
+    return true;
+  }
+  else if(Compare(T, Division(a,c),b)==true){
+    return true;
+  }
+  else if(Compare(T, Subtraction(a,b),c)==true){
+    return true;
+  }
+  else if(Compare(T, Subtraction(b,c),a)==true){
+    return true;
+  }
+  else if(Compare(T, Subtraction(a,c),b)==true){
+    return true;
+  }
+  else {
+    return false;
+  }
+
+}
+int main() {
+	int a = 0;
+	int b = 0;
+  int c = 0;
+  int max =0; 
+
+  ofstream outfile;
+  outfile.open ("output.txt");
+  ofstream out;
+  out.open ("out.txt");
+
+  Countdown C;
+  
+  ifstream infile;
+	infile.open("input.txt");
+	if (!infile) {
+		exit(1);
+	} 
+  else {
+    while (infile >> a >> b >> c) {
+		  int count =0;
+      out<< "{ " << a << " , " << b << " , "<<  c << "  }"<< endl;
+      outfile<< "{ " << a << " , " << b << " , "<<  c << "  }"<< endl;
+      for(int x=101;x<=999; x++){
+       if( C.Compare(x,a,b,c) == true){
+         count++;
+          outfile<< "     This solves "<< x << endl;
+        }
+      }
+      out << "        Number Solved  :" << count << endl;
+      if ( count >= max){
+        max =count;
+      }
+      }
+      out <<endl;
+      out << " The max targets solved is  " << max << endl;
+	  }
+}
